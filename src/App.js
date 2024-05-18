78cd2df9f94e3245632ae6f926c3748fe1bad743
Monika Derewecka <monika.derewecka@schibsted.com>
2024-05-18 15:02:31 +0200

    add logging

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import {useState} from "react";
 
 function App() {
     const [email, setEmail] = useState('monika@gmail.com');
+    const [state, setState] = useState(false);
+
+    let content;
+    if (!state) {
+        content = <div>
+            Zaloguj się e-mailem
+                <input type="text" onChange={handleChange} />
+                <button type="button" onClick={() => setState(true)}>
+                    Wchodzę
+                </button>
+        </div>
+    } else {
+        content = <div>
+            <h1>Twój e-mail to {email}</h1>
+        </div>
+    }
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
 
-    let message;
-    if (email.length < 10) {
-        message = <div>Email jest krótszy niż 5 znaków</div>
-    } else if (email.length < 25) {
-        message = <div>Email jest w sam raz</div>
-    } else {
-        message = <div>Email jest za długi</div>
-    }
-
     return (
         <div>
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}</h2>
-            <input type="text" onChange={handleChange} />
-            {message}
+            <h1>Witaj w systemie zapisów na zajęcia</h1>
+            {content}
         </div>
     );
 }
