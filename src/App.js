dbb7af3a339a35e4b3fa708b1db6bb3848a5acd4
Monika Derewecka <monika.derewecka@schibsted.com>
2024-05-18 14:06:57 +0200

    add email length check

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'monika@gmail.com';
+    const [email, setEmail] = useState('monika@gmail.com');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if (email.length < 10) {
+        message = <div>Email jest krótszy niż 5 znaków</div>
+    } else if (email.length < 15) {
+        message = <div>Email jest w sam raz</div>
+    } else {
+        message = <div>Email jest za długi</div>
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email.toUpperCase()}</h2>
+            <h2>Twój e-mail to {email}</h2>
             <input type="text" onChange={handleChange} />
+            {message}
         </div>
     );
 }
